5cff2bea9a8153e811fec4de413532bde5087a8b
Ivy Sifuma <ivy.sifuma@student.moringaschool.com>
2022-10-07 14:53:31 +0300

    Bot

--- src/components/BotsPage.js
+++ src/components/BotsPage.js
@@ -5,20 +5,20 @@ import BotCollection from "./BotCollection";
 
 function BotsPage() {
   //start here with your code for step one
-  const [botData, setBotData] = useState([]);
+  const [botDetails, setBotDetails] = useState([]);
   const [botArmy, setBotArmy] = useState([]);
 
   useEffect(() => {
     fetch("http://localhost:8002/bots")
       .then((res) => res.json())
-      .then((data) => setBotData(data));
+      .then((details) => setBotDetails(details));
   }, []);
   //console.log(botData)
 
   return (
     <div>
-      <YourBotArmy botArmy={botArmy} setBotArmy={setBotArmy} setBotData={setBotData}/>
-      <BotCollection botArmy={botArmy} setBotArmy={setBotArmy} bots={botData} setBotData={setBotData} />
+      <YourBotArmy botArmy={botArmy} setBotArmy={setBotArmy} setBotDetails={setBotDetails}/>
+      <BotCollection botArmy={botArmy} setBotArmy={setBotArmy} bots={botDetails} setBotData={setBotDetails} />
     </div>
   );
 }
