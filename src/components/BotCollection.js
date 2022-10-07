8205068ed4246710146e708da2b645bf27be98cc
Ivy Sifuma <ivy.sifuma@student.moringaschool.com>
2022-10-07 13:15:43 +0300

    Bot

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,22 +1,42 @@
 import React from "react";
 import BotCard from "./BotCard";
 
-
-  function BotCollection({ bots, botArmy, setBotArmy, setBotData, setActiveBot }) {
+function BotCollection({bots, addBotToArmy,setBots, setActiveBot, removeBotFromArmy}) {
   // Your code here
+  function deleteBot(deleteBot){
+    const UpdatedBots = bots.filter((bot)=>{
+      return bot.id !==deleteBot.id
+    })
+    setBots(UpdatedBots)
+  }
+
+function BotCollection({bots, addBotToArmy,deleteBot }) {
+
   return (
     <div className="ui four column grid">
       <div className="row">
         {/*...and here..*/}
+        {bots.map((bot)=>{
+          return(
+            <BotCard
+            bot={bot}
+            key={bot.id}
+            addBotToArmy={addBotToArmy}
+            onDelete ={deleteBot}
+            setActiveBots={setActiveBot}
+            removeBotFromArmy={removeBotFromArmy}
+
+
+              
+            />
+          );
+        })}
+      
         Collection of all bots
-
-        {bots.map((bot) => (
-          <BotCard key={bot.id} bot={bot} botArmy={botArmy} setBotArmy={setBotArmy} setBotData={setBotData} setActiveBot={setActiveBot}/>
-
-          ))}
       </div>
     </div>
   );
+}
 }
 
 export default BotCollection;
