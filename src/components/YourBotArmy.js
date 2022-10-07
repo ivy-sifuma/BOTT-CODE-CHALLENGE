02b53c5797d8833cca9edc3d3faa841d8ace78ba
Ivy Sifuma <ivy.sifuma@student.moringaschool.com>
2022-10-07 14:48:37 +0300

    Bot

--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -1,35 +1,25 @@
 import React from "react";
 import BotCard from "./BotCard";
 
-function YourBotArmy({yourBots,setYourBots,removeFromBot}) {
-function YourBotArmy({yourBots,setYourBots, deleteBot}) {
+function YourBotArmy({setBotArmy, botArmy}) {
   //your bot army code here...
-  function removeBotFromArmy(bot) {
-    const newArmy = yourBots.filter((b) => b.id !== bot.id);
-    setYourBots(newArmy);
-  }
+
   return (
     <div className="ui segment inverted olive bot-army">
       <div className="ui five column grid">
         <div className="row bot-army-row">
           {/*...and here...*/}
-          {yourBots.map((bot) =>{
-            return(
-              <BotCard
-              bot={bot}
-              key={bot.id}
-              handleBot={removeFromBot}
-             removeBotFromArmy={removeBotFromArmy}
-             deleteBot={deleteBot}
-              isInArmy ={true}
-              />
-            )
-          })}
           Your Bot Army
+          {botArmy.map((bot => 
+            <BotCard key={bot.id} bot={bot} setBotArmy={setBotArmy} botArmy={botArmy} />
+            
+            ))}
+
+
         </div>
       </div>
     </div>
   );
-}}
+}
 
 export default YourBotArmy;
