d8422c9aa8959ed3de191fba821c89a3d4da4ddd
Ivy Sifuma <ivy.sifuma@student.moringaschool.com>
2022-10-07 14:27:01 +0300

    Bot

--- src/components/BotsPage.js
+++ src/components/BotsPage.js
@@ -1,47 +1,22 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
+
 import YourBotArmy from "./YourBotArmy";
 import BotCollection from "./BotCollection";
-import BotSpecs from "./BotSpecs";
 
 function BotsPage() {
   //start here with your code for step one
-function BotsPage() {
-    setYourBots([...yourBots,bot]);
-  }
+  const [botData, setBotData] = useState([]);
+  const [botArmy, setBotArmy] = useState([]);
 
-  function removeFromBot(bot) {
-    setYourBots(yourBots.filter(bots=>bots!==bot))
-  } 
-  function deleteBot(bot) {
-    const updateYourBots = yourBots.filter((b) => b.id !== bot.id);
-    const updateBots = bots.filter((b) => b.id !== bot.id);
-
-    fetch(`http://localhost:8002/bots/${bot.id}`, {
-      method: "DELETE",
-    }).then(() => {
-      setYourBots(updateYourBots)
-      setBots(updateBots)})
-  }
+  useEffect(() => {
+    fetch("http://localhost:8002/bots")
+      .then((res) => res.json())
+      .then((data) => setBotData(data));
+  }, []);
+  //console.log(botData)
 
   return (
     <div>
-      <YourBotArmy
-      bots={bots}
-      yourBots={yourBots}
-      setYourBots={setYourBots}
-      removeFromBot={removeFromBot}
-      deleteBot={deleteBot}
-      />
-
-     <BotCollection 
-      bots ={bots}
-      addBotToArmy={addBotToArmy}
-      setBots={setBots}
-      deleteBot={deleteBot}
-      />
-    </div>
-  )
-}
-
+      <Your
 
 export default BotsPage;
